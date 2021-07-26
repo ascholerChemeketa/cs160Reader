7fc2e055b5a9b9927fdcad9953bc023d3f12bd3f
Andrew Scholer <ascholer@chemeketa.edu>
2021-07-26 13:16:13 -0700

    Update conf.py and pavement.py to match more modern Runestone project settings

--- pavement.py
+++ pavement.py
@@ -1,60 +1,52 @@
-import paver
-from paver.easy import *
+import os
+import sys
+import pkg_resources
+from socket import gethostname
+
+from paver.easy import options, Bunch
 import paver.setuputils
+
+from runestone import get_master_url
+from runestone import build  # NOQA: F401 -- build is called implicitly by the paver driver.
+from runestone.server import get_dburl
+
 paver.setuputils.install_distutils_tasks()
-import os, sys
-
-from sphinxcontrib import paverutils
-
 sys.path.append(os.getcwd())
 
 home_dir = os.getcwd()
+
 master_url = 'http://127.0.0.1:8000'
-master_app = 'CS160Reader'
-serving_dir = "../built/CS160Reader"
-
-
-minify_js = True
-minifiedjs = True
+project_name = 'CS160Reader'
+serving_dir = './built/CS160Reader'
 
 options(
     sphinx = Bunch(docroot=".",),
 
-
-	# build = Bunch(
-    #     builddir="../builtlatex/CS160Reader",
-    #     sourcedir="_sources",
-    #     outdir="../builtlatex/CS160Reader",
-    #     builder='latex',
-    #     confdir=".",
-    #     project_name = "CS160Reader",
-    #     template_args={'course_id': 'CS160Reader',
-    #                    'login_required':'false',
-    #                    'appname':master_app,
-    #                    'loglevel': 0,
-    #                    'course_url':master_url,
-    #                    'use_services': 'false',
-    #                    'python3': 'true'
-    #                     }
-    # ),
-
     build = Bunch(
-        builddir="./built/CS160Reader",
-        sourcedir="_sources",
-        outdir="./built/CS160Reader",
-        confdir=".",
-        project_name = "CS160Reader",
-        template_args={'course_id': 'CS160Reader',
-                       'login_required':'false',
-                       'appname':master_app,
-                       'loglevel': 0,
-                       'course_url':master_url,
-                       'use_services': 'false',
-                       'python3': 'true'
-                        }
+        builddir = serving_dir,
+        sourcedir = "_sources",
+        outdir = serving_dir,
+        confdir = ".",
+        template_args={
+            'login_required':'false',
+            'loglevel': 0,
+            'course_title': project_name,
+            'python3': 'true',
+            'dburl': '',
+            'default_ac_lang': 'python',
+            'downloads_enabled': 'false',
+            'enable_chatcodes': 'false',
+            'allow_pairs': 'false',
+            'dynamic_pages': False,
+            'use_services': 'false',
+            'basecourse': project_name,
+            'course_id': project_name,
+            # These are used for non-dynamic books.
+            'appname': project_name,
+            'course_url': master_url,
+        }
     ),
-
-
 )
 
-from runestone import build  # build is called implicitly by the paver driver.
+version = pkg_resources.require("runestone")[0].version
+options.build.template_args['runestone_version'] = version
