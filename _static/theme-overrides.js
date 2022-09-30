88ff6bde16b47bc1a18d47b80820eff97eecc983
Andrew Scholer <ascholer@chemeketa.edu>
2022-09-30 07:39:36 -0700

    Logo override update

--- _static/theme-overrides.js
+++ _static/theme-overrides.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let logo;
     let logos = document.getElementsByClassName("brand-logo");
-    let logo = logos[0];
+    if(logos)
+        logo = logos[0];
 
     let brands = document.getElementsByClassName("navbar-brand");
-    let brand = brands[0];
+    if(brands) {
+        let brand = brands[0];
+        brand.setAttribute('title', brand.innerHTML);
+        brand.innerHTML = "Contents";
 
-    brand.setAttribute('title', brand.innerHTML);
-    brand.innerHTML = "Contents";
-
-    logo.href = brand.href;
+        if(logo !== undefined) {
+            logo.href = brand.href;
+        }
+    }
 });
