2b1f86d6d004eeb2a6a008574f4ac1290a51f846
Andrew Scholer <ascholer@chemeketa.edu>
2023-03-08 20:26:19 -0800

    Monkeypatch to ajax lint

--- _static/theme-overrides.js
+++ _static/theme-overrides.js
@@ -20,3 +20,37 @@ document.addEventListener('DOMContentLoaded', function() {
     if(warning)
         warning.classList.add("red");
 });
+
+//Add listener to do ajax call when python code is run
+document.addEventListener('DOMContentLoaded', function() {
+    //wait for runestone to load components
+    setTimeout(() => {
+        const pythonACs = document.querySelectorAll('div[lang="python"]');
+        pythonACs.forEach((acItem) => {
+            const runBtn = acItem.querySelector("button.run-button");
+            const codeMirror = acItem.querySelector("div.CodeMirror");
+            runBtn.addEventListener('click', () => {
+                let currentCode = codeMirror.CodeMirror.getValue();
+                var data = new FormData();
+                data.append('code', currentCode);
+                fetch('https://postman-echo.com/post', {
+                    method: 'POST',
+                    body: JSON.stringify(currentCode),
+                    mode: 'no-cors'
+                })
+                .then((response) => {
+                    console.log(response);
+                    const stdOutPre = acItem.querySelector("div.ac_output pre");
+                    stdOutPre.insertAdjacentHTML("afterbegin", "")
+                    //response.json();
+                })
+                // .then((jsonData) => {
+                //     console.log(jsonData);
+                // })
+                .catch(err => {
+                    console.log("Error w/ajax");
+                });
+            });
+        });
+    }, 2000);
+});
