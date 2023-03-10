d5d234c3884640cadd5cc59f19c11267d9405965
Andrew Scholer <ascholer@chemeketa.edu>
2023-03-10 11:37:36 -0800

    Fix linter attaching race condition

--- _static/theme-overrides.js
+++ _static/theme-overrides.js
@@ -21,44 +21,54 @@ document.addEventListener('DOMContentLoaded', function() {
         warning.classList.add("red");
 });
 
-//Add listener to do ajax call when python code is run
-document.addEventListener('DOMContentLoaded', function() {
-    //wait for runestone to load components
-    setTimeout(() => {
-        const pythonACs = document.querySelectorAll('div[lang="python"]');
-        pythonACs.forEach((acItem) => {
-            const runBtn = acItem.querySelector("button.run-button");
-            const codeMirror = acItem.querySelector("div.CodeMirror");
-            runBtn.addEventListener('click', () => {
-                let currentCode = codeMirror.CodeMirror.getValue();
-                var data = new FormData();
-                data.append('code', currentCode);
-                fetch('http://157.230.167.85:4242/test', {
-                    method: 'POST',
-                    body: JSON.stringify(currentCode)
-                })
-                .then((response) => {
-                    console.log(response);
-                    return response.text();
-                })
-                .then((data) => {
-                    let lintPre = acItem.querySelector("div.ac_output pre.lint-results");
-                    if( lintPre == null ) {
-                        const stdOutPre = acItem.querySelector("div.ac_output pre");
-                        stdOutPre.insertAdjacentHTML("beforebegin", '<pre class="lint-results"></pre>');
-                        lintPre = acItem.querySelector("div.ac_output pre.lint-results");
-                    }
-                    if(data.trim() !== '') {
-                        lintPre.classList.remove("hidden");
-                        lintPre.innerHTML = "Syntax warnings:\n" + data;
-                    } else {
-                        lintPre.classList.add("hidden");
-                    }
-                })
-                .catch(err => {
-                    console.log("Error w/ajax");
-                });
+function attachLinter() {
+    //check to see if active code items have been set up
+    const unproccessedACs = document.querySelectorAll('textarea[data-lang="python"]');
+    if(unproccessedACs.length > 0) {
+        setTimeout(() => {
+            attachLinter();
+        }, 1000);
+        return;
+    }
+    //OK, good to go
+    const pythonACs = document.querySelectorAll('div[lang="python"]');
+    pythonACs.forEach((acItem) => {
+        const runBtn = acItem.querySelector("button.run-button");
+        const codeMirror = acItem.querySelector("div.CodeMirror");
+        runBtn.addEventListener('click', () => {
+            let currentCode = codeMirror.CodeMirror.getValue();
+            var data = new FormData();
+            data.append('code', currentCode);
+            fetch('http://157.230.167.85:4242/test', {
+                method: 'POST',
+                body: JSON.stringify(currentCode)
+            })
+            .then((response) => {
+                console.log(response);
+                return response.text();
+            })
+            .then((data) => {
+                let lintPre = acItem.querySelector("div.ac_output pre.lint-results");
+                if( lintPre == null ) {
+                    const stdOutPre = acItem.querySelector("div.ac_output pre");
+                    stdOutPre.insertAdjacentHTML("beforebegin", '<pre class="lint-results"></pre>');
+                    lintPre = acItem.querySelector("div.ac_output pre.lint-results");
+                }
+                if(data.trim() !== '') {
+                    lintPre.classList.remove("hidden");
+                    lintPre.innerHTML = "Syntax warnings:\n" + data;
+                } else {
+                    lintPre.classList.add("hidden");
+                }
+            })
+            .catch(err => {
+                console.log("Error w/ajax");
             });
         });
-    }, 1000);
+    });
+}
+
+//Add listener to do ajax call when python code is run
+document.addEventListener('DOMContentLoaded', function() {
+    attachLinter();
 });
